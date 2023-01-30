dd1c47a5ca45237df15d9dd719d4b799f08c397c
Cesar Araya <nobi.car@gmail.com>
2023-01-30 20:20:45 -0300

    add stats

--- src/components/PokeData.jsx
+++ src/components/PokeData.jsx
@@ -3,16 +3,22 @@ import React from 'react'
 const PokeData = ({ selectedPokemon }) => {
   if (!selectedPokemon) return <p>...</p>
 
-  const { id, name, sprites } = selectedPokemon
+  const { id, name, sprites, stats } = selectedPokemon
 
   return (
-    <>
-      <div>{id}</div>
-      <div>{name}</div>
-      <img src={sprites?.front_default} alt='Selected Pokemon' />
-      <img src={sprites?.other?.dream_world.front_default} alt='Selected Pokemon' />
-      <img src={sprites?.other?.home.front_default} alt='Selected Pokemon' />
-    </>
+    <div className='flex flex-wrap'>
+      <div>
+        <img className='h-36 w-36' src={sprites?.other?.dream_world.front_default} alt='Selected Pokemon' />
+      </div>
+      <div>
+        <p>Nº {id}</p>
+        <p>Nombre: {name}</p>
+        <p className='border-2'>Estadisticas {stats?.map((m) => {
+          return (<p key={m.stat.url}>{m.stat.name}: {m.base_stat}</p>)
+        })}
+        </p>
+      </div>
+    </div>
   )
 }
 
