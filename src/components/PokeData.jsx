00edf5d81983f1dd0ee9d4f015dbfd127bde9e75
Cesar <nobi.car@gmail.com>
2023-06-28 18:46:18 -0400

    test

--- src/components/PokeData.jsx
+++ src/components/PokeData.jsx
@@ -13,7 +13,7 @@ const PokeData = ({ selectedPokemon, setSelectedPokemon }) => {
   if (!selectedPokemon.id) {
     return (
       <div className='flex flex-wrap place-content-evenly'>
-        <Image src={ghost} alt='no pokemon selected' />
+        <Image src={ghost} alt='No pokemon selected' />
         <PokeList setSelectedPokemon={setSelectedPokemon} />
       </div>
     )
